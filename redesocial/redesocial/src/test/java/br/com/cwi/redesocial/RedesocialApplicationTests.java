c0959c17b90bdeb958359495a2fe25fa26f0c24a
SaraFinkler <kulzerfinklersara@gmail.com>
2026-05-27 17:25:16 -0300

    criação do projeto

---
+++ redesocial/redesocial/src/test/java/br/com/cwi/redesocial/RedesocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cwi.redesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
